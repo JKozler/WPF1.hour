90296c5b434001848e197778e99155551cf30989
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-01-20 16:24:49 +0100

    Přidejte soubory projektu.

---
+++ FirstProject/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstProject
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Konec_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            if (tBName.Text != null)
+            {
+                Pozdrav.Content = "Ahoj náš " + tBName.Text + "!";
+                Back.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Pozdrav.Content = "";
+            Back.Visibility = Visibility.Hidden;
+            tBName.Text = "";
+        }
+    }
+}
